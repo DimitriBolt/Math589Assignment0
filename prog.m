a23ba53ac677661878888d80ea84b7335f124069
Dimitri Bolt <Dmitry@Bolt.Investments>
2024-08-28 23:30:27 -0700

    double a,b,c

--- prog.m
+++ prog.m
@@ -3,6 +3,9 @@ function x = prog(a, b, c)
     % Returns the roots of the equation
     
     % Calculate the discriminant
+    a = double(a);
+    b = double(b);
+    c = double(c);
     D = double(b^2 - 4*a*c);
     
     x(1) = double(-b + sqrt(D)) / (2*a);
